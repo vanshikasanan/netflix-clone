2a5bf88dccbd6874b4ac945bc116bc962090f200
vanshikasanan <64725289+vanshikasanan@users.noreply.github.com>
2021-08-12 13:16:05 +0530

    final commit

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,16 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC4hW9KdgiEIKtgWL9sUrDrz8Bj7gUXA_4",
-    authDomain: "netflix-clone-fa67b.firebaseapp.com",
-    projectId: "netflix-clone-fa67b",
-    storageBucket: "netflix-clone-fa67b.appspot.com",
-    messagingSenderId: "452018920438",
-    appId: "1:452018920438:web:c3506b4ef1a4807d9185d7"
-  };
+  apiKey: "AIzaSyCdRmJd3ckM0j047Q6rWc2MZwjC8jeP9xQ",
+  authDomain: "netflix-clone-4c932.firebaseapp.com",
+  projectId: "netflix-clone-4c932",
+  storageBucket: "netflix-clone-4c932.appspot.com",
+  messagingSenderId: "24769342810",
+  appId: "1:24769342810:web:898ecbe267ee725e7fd763"
+};
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
+const db = firebaseApp.firestore();
 const auth = firebase.auth();
 
 export{ auth };
